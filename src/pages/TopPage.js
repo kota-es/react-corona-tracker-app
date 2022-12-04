c06516201a1cbf793cfed1670c45da7a6ecaabd5
kota-es <info@k-3110.com>
2022-12-04 20:51:17 +0900

    make header

--- src/pages/TopPage.js
+++ src/pages/TopPage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Title from '../components/Title'
 import Selector from '../components/Selector'
 import Results from '../components/Results'
+import Header from '../components/Header'
 
 const TopPage = (props) => {
   const {countriesJson, setCountry, getCountryData, countryData } = props
   return (
     <div>
+      <Header />
       <Title />
       <Selector countriesJson={countriesJson} setCountry={setCountry} getCountryData={getCountryData} />
       <Results countryData={countryData} />
--- src/pages/WorldPage.js
+++ src/pages/WorldPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Title from '../components/Title'
 import Card from '../components/Card'
+import Header from '../components/Header'
 
 const WorldPage = (props) => {
   const { allCountriesData } = props
   return (
     <div>
+      <Header />
       <Title />
       <Card allCountriesData={allCountriesData} />
     </div>
