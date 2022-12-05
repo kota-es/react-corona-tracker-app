cee4aa7fe747c0746aa3c4d4a4ff4bebc932a922
kota-es <info@k-3110.com>
2022-12-05 21:50:17 +0900

    fix digit

--- src/components/Results.js
+++ src/components/Results.js
@@ -6,13 +6,16 @@ const Results = ({countryData, loading}) => {
   return (
     <>
     {loading ? <Loading /> :
-      <div>
-        <p>日付： { date.slice(0, 10) }</p>
-        <p>新規感染者： { newConfirmed.toLocaleString() }</p>
-        <p>感染者総数： { totalConfirmed.toLocaleString() }</p>
-        <p>新規回復者： { newRecovered.toLocaleString() }</p>
-        <p>回復者総数： { totalRecovered.toLocaleString() }</p>
-      </div>
+    <>
+    <div>
+    <p>日付： { date.slice(0, 10) }</p>
+    <p>新規感染者： { newConfirmed.toLocaleString() }</p>
+    <p>感染者総数： { totalConfirmed.toLocaleString() }</p>
+    <p>新規回復者： { newRecovered.toLocaleString() }</p>
+    <p>回復者総数： { totalRecovered.toLocaleString() }</p>
+  </div>
+  <div>re</div>
+  </>
     }
     </>
   )
