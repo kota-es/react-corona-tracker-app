a98ea861ef05aed38e903c5c05bccdea430ac366
kota-es <info@k-3110.com>
2022-12-07 08:35:41 +0900

    TopPage to ts

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -4,7 +4,23 @@ import Selector from '../components/Selector'
 import Results from '../components/Results'
 import Header from '../components/Header'
 
-const TopPage = ({countriesJson, setCountry, countryData, loading}) => {
+type TopPageType = {
+  countriesJson: {
+    Country: string,
+    Slug: string,
+  }[],
+  setCountry: React.Dispatch<React.SetStateAction<string>>,
+  countryData:{
+    date: string,
+    newConfirmed: number,
+    totalConfirmed: number,
+    newRecovered: number,
+    totalRecovered: number,
+  },
+  loading: boolean,
+}
+
+const TopPage = ({countriesJson, setCountry, countryData, loading}: TopPageType) => {
   return (
     <div>
       <Header />
